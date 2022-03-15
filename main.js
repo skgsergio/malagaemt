e788da0722d8bad31a7b490550d2158f08cc6826
Sergio Conde <skgsergio@gmail.com>
2022-03-15 13:45:31 +0100

    Update all dependencies

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 'use strict';
 
-const console = require('console');
+import console from 'console';
+import fetch from 'node-fetch';
+import express from 'express';
+import morgan from 'morgan';
+import apicache from 'apicache';
 
-const request = require('request');
-const express = require('express');
-const morgan = require('morgan');
-const apicache = require('apicache');
+import app_pkg from './package.json' assert { type: 'json' };
 
 const config = {
     bind: process.env.BIND || '127.0.0.1',
     port: process.env.PORT || 8080,
     dataurl: 'https://datosabiertos.malaga.eu/recursos/transporte/EMT/EMTlineasUbicaciones/lineasyubicaciones.geojson'
 };
+
+const {pathname: viewsdir} = new URL('./views/', import.meta.url)
 
 const app = express();
 
@@ -25,16 +28,18 @@ apicache.options({
 app.use(morgan('common'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(viewsdir + 'index.html');
 });
 
 app.get('/data.geojson', apicache.middleware('1 minute'), (req, res) => {
-    request({ url: config.dataurl, headers: { 'User-Agent': 'malagaemt/0.0.0' } })
-        .on('error', (err) => {
-            console.log(err);
-            res.status(500).send({ error: err });
+    fetch(config.dataurl, { headers: { 'User-Agent': `${app_pkg.name}/${app_pkg.version}` } })
+        .then(response => {
+            response.body.pipe(res);
         })
-        .pipe(res);
+        .catch(err => {
+            console.log(err);
+            res.status(500).send({ error: "Failed fetching remote data." });
+        });
 });
 
 app.listen(config.port, config.bind, () => console.log(`Server running on http://${config.bind}:${config.port}...`));
